4bf99f01176ecb70dfa27e4b23275ac339c9f3de
iluca <lucamodel96@gmail.com>
2018-01-17 22:18:13 +0100

    aggiornamento

--- Tirocinio2.5/src/main/java/gestioneUtente/control/TirocinioControl.java
+++ Tirocinio2.5/src/main/java/gestioneUtente/control/TirocinioControl.java
@@ -1,5 +1,11 @@
 package gestioneUtente.control;
 
+import gestioneProfessore_TutorAziendale.model.ProfessoreTutorAziendale;
+import gestioneSegreteria.model.Segreteria;
+import gestioneStudente.model.Studente;
+import gestioneUtente.model.Tirocinio;
+import gestioneUtente.model.TirocinioModel;
+
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -9,273 +15,243 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import gestioneUtente.model.*;
-import gestioneStudente.model.*;
-import gestioneProfessore_TutorAziendale.model.*;
-import gestioneSegreteria.model.*;
-
 /**
- * 
- * TirocinioControl è una classe con 2 metodi e permeti di cancellare/convalidare/confermare/rifiutare/accettare una domanda di tirocinio. 
+ * TirocinioControl è una classe con 2 metodi e permeti di 
+ * cancellare/convalidare/confermare/rifiutare/accettare una domanda di tirocinio. 
  * 
  */
 public class TirocinioControl extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	
-	String return_path = "/index.jsp";
-	
-	public TirocinioControl() {
-		super();
-	}
+  private static final long serialVersionUID = 1L;
 
-	/**
-	 * Il metodo doGet cerca di cancellare/convalidare/confermare/rifiutare/accettare una domanda di tirocinio. 
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
 
-		String action = request.getParameter("action");
-		
-	    TirocinioModel Tirociniomodel = null;
-	
-		try {
-			if (action != null) {
-				
-				if (action.equalsIgnoreCase("delete_training_student"))
-				{
-					Studente sessione_student = (Studente) request.getSession().getAttribute("student");
-					
-					 if(sessione_student!=null)		
-						 if(sessione_student.getEmail().length()>0)
-						 {
-							String id = request.getParameter("id");
-							int id_int = Integer.parseInt(id);
-									
-							if(id != null)
-							{
-								Tirociniomodel = new TirocinioModel();
-								
-								Tirocinio tirocinio = (Tirocinio) Tirociniomodel.doRetrieveByKey(id_int);
-								
-								if(tirocinio.getStato().equalsIgnoreCase("In Attesa"))
-								{
-									Tirociniomodel.doDelete(id_int);
-									request.setAttribute("message_success_training", "Richiesta di Tirocinio Cancellata.");
-								}
-								else
-								{
-									request.setAttribute("message_fault_training", "Per cancellare la richiesta di Tirocinio lo stato deve essere In Attesa.");
-								}
-								
-								return_path = "/MyTraining.jsp";	
-							}
-						 }		    
-				}
-				else
-					if (action.equalsIgnoreCase("confirm_student_training_student"))
-					{
-						Studente sessione_student = (Studente) request.getSession().getAttribute("student");
-						
-						 if(sessione_student!=null)		
-							 if(sessione_student.getEmail().length()>0)
-							 {
-								String id = request.getParameter("id");
-								int id_int = Integer.parseInt(id);
-										
-								if(id != null)
-								{
-									Tirociniomodel = new TirocinioModel();
-									
-									Tirocinio tirocinio = (Tirocinio) Tirociniomodel.doRetrieveByKey(id_int);
-									
-									if(tirocinio.getStato().equalsIgnoreCase("Accettata"))
-									{
-										Tirociniomodel.doModify("Confermata",id_int);
-										request.setAttribute("message_success_training", "Richiesta di Tirocinio è stata Confermata.");
-									}
-									else
-									{
-										request.setAttribute("message_fault_training", "Per confermare la richiesta di Tirocinio lo stato deve essere in Accettata.");
-									}
-									
-									return_path = "/MyTraining.jsp";	
-								}
-							 }		    
-					}
-					else
-					if (action.equalsIgnoreCase("reject_student_training_student"))
-					{
-						Studente sessione_student = (Studente) request.getSession().getAttribute("student");
-						
-						 if(sessione_student!=null)		
-							 if(sessione_student.getEmail().length()>0)
-							 {
-								String id = request.getParameter("id");
-								int id_int = Integer.parseInt(id);
-										
-								if(id != null)
-								{
-									Tirociniomodel = new TirocinioModel();
-									
-									Tirocinio tirocinio = (Tirocinio) Tirociniomodel.doRetrieveByKey(id_int);
-									
-									if(tirocinio.getStato().equalsIgnoreCase("Accettata"))
-									{
-										Tirociniomodel.doModify("Rifiutata dallo Studente",id_int);
-										request.setAttribute("message_success_training", "Richiesta di Tirocinio è stata Rifiutata.");
-									}
-									else
-									{
-										request.setAttribute("message_fault_training", "Per rifiutare la richiesta di Tirocinio lo stato deve essere in Accettata.");
-									}
-									
-									return_path = "/MyTraining.jsp";	
-								}
-							 }		    
-					}
-					else
-						if (action.equalsIgnoreCase("accept_training_student"))
-						{
-							ProfessoreTutorAziendale sessione_teacher = (ProfessoreTutorAziendale) request.getSession().getAttribute("teacher");
-							ProfessoreTutorAziendale sessione_tutor = (ProfessoreTutorAziendale) request.getSession().getAttribute("tutor");
-							
-							boolean control = false;
-							
-							if(sessione_teacher!=null)		
-								 if(sessione_teacher.getEmail().length()>0)
-								 {
-									 control = true;
-								 }
-							
-							if(sessione_tutor!=null)		
-								 if(sessione_tutor.getEmail().length()>0)
-								 {
-									 control = true;
-								 }
-							
-							if(control)
-							{
-								String id = request.getParameter("id");
-								int id_int = Integer.parseInt(id);
-										
-								if(id != null)
-								{
-									Tirociniomodel = new TirocinioModel();
-									
-									Tirocinio tirocinio = (Tirocinio) Tirociniomodel.doRetrieveByKey(id_int);
-									
-									if(tirocinio.getStato().equalsIgnoreCase("In Attesa"))
-									{
-										Tirociniomodel.doModify("Accettata",id_int);
-										request.setAttribute("message_success_training", "Richiesta di Tirocinio Accettata.");
-									}
-									else
-									{
-										request.setAttribute("message_fault_training", "Per accettare la richiesta di Tirocinio lo stato deve essere In Attesa.");
-									}
-									
-									return_path = "/RequestTraining.jsp";	
-								}
-							}
-						}
-						else
-							if (action.equalsIgnoreCase("reject_training_student"))
-							{
-								ProfessoreTutorAziendale sessione_teacher = (ProfessoreTutorAziendale) request.getSession().getAttribute("teacher");
-								ProfessoreTutorAziendale sessione_tutor = (ProfessoreTutorAziendale) request.getSession().getAttribute("tutor");
-								
-								boolean control = false;
-								
-								if(sessione_teacher!=null)		
-									 if(sessione_teacher.getEmail().length()>0)
-									 {
-										 control = true;
-									 }
-								
-								if(sessione_tutor!=null)		
-									 if(sessione_tutor.getEmail().length()>0)
-									 {
-										 control = true;
-									 }
-								
-								if(control)
-								{
-									String id = request.getParameter("id");
-									int id_int = Integer.parseInt(id);
-											
-									if(id != null)
-									{
-										Tirociniomodel = new TirocinioModel();
-										
-										Tirocinio tirocinio = (Tirocinio) Tirociniomodel.doRetrieveByKey(id_int);
-										
-										if(tirocinio.getStato().equalsIgnoreCase("In Attesa"))
-										{
-											Tirociniomodel.doModify("Rifiutata",id_int);
-											request.setAttribute("message_success_training", "Richiesta di Tirocinio Rifiutata.");
-										}
-										else
-										{
-											request.setAttribute("message_fault_training", "Per rifiutare la richiesta di Tirocinio lo stato deve essere In Attesa.");
-										}
-										
-										return_path = "/RequestTraining.jsp";	
-									}
-								}
-							}
-							else
-								if (action.equalsIgnoreCase("convalidate_training_student"))
-								{
-									Segreteria sessione_segreteria = (Segreteria) request.getSession().getAttribute("segreteria");
-									
-									boolean control = false;
-									
-									if(sessione_segreteria!=null)		
-										 if(sessione_segreteria.getEmail().length()>0)
-										 {
-											 control = true;
-										 }
-									
-									if(control)
-									{
-										String id = request.getParameter("id");
-										int id_int = Integer.parseInt(id);
-												
-										if(id != null)
-										{
-											Tirociniomodel = new TirocinioModel();
-											
-											Tirocinio tirocinio = (Tirocinio) Tirociniomodel.doRetrieveByKey(id_int);
-											
-											if(tirocinio.getStato().equalsIgnoreCase("Completato"))
-											{
-												Tirociniomodel.doModify("Convalidato",id_int);
-												request.setAttribute("message_success_training", "Tirocinio Convalidato.");
-											}
-											else
-											{
-												request.setAttribute("message_fault_training", "Per convalidare il Tirocinio lo stato deve essere in Completato.");
-											}
-											
-											return_path = "/segreteria/CompletedTraining.jsp";	
-										}
-									}
-								}
-			}
-		} catch (SQLException e) {
-			System.out.println("Error:" + e.getMessage());	      
-		}	
-		
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(return_path);
-		dispatcher.forward(request, response);
-	}
+  String returnPath = "/index.jsp";
 
-	/**
-	 * Il metodo riceve una richiesta dal client e richiama il medoto doGet(request, response) per svolgere la procedura aspettata.
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doGet(request, response);
-	}
+  public TirocinioControl() {
+    super();
+  }
+
+  /**
+  * Il metodo doGet cerca di cancellare/convalidare/confermare/rifiutare/accettare
+  * una domanda di tirocinio. 
+  */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    String action = request.getParameter("action");
+
+    TirocinioModel tirocinioModel = null;
+
+    try {
+      if (action != null) {
+
+        if (action.equalsIgnoreCase("delete_training_student")) {
+          Studente sessioneStudent = (Studente) request.getSession().getAttribute("student");
+
+          if (sessioneStudent != null) {
+            if (sessioneStudent.getEmail().length() > 0) {
+              String id = request.getParameter("id");
+              int idInt = Integer.parseInt(id);
+
+              if (id != null) {
+                tirocinioModel = new TirocinioModel();
+
+                Tirocinio tirocinio = (Tirocinio) tirocinioModel.doRetrieveByKey(idInt);
+
+                if (tirocinio.getStato().equalsIgnoreCase("In Attesa")) {
+                  tirocinioModel.doDelete(idInt);
+                  request.setAttribute("message_success_training", ""
+                      + "Richiesta di Tirocinio Cancellata.");
+                } else {
+                  request.setAttribute("message_fault_training", "Per cancellare "
+                      + "la richiesta di Tirocinio lo stato deve essere In Attesa.");
+                }
+
+                returnPath = "/MyTraining.jsp";
+              }
+            }
+          }
+        } else if (action.equalsIgnoreCase("confirm_student_training_student")) {
+          Studente sessioneStudent = (Studente) request.getSession().getAttribute("student");
+
+          if (sessioneStudent != null) {
+            if (sessioneStudent.getEmail().length() > 0) {
+              String id = request.getParameter("id");
+              int idInt = Integer.parseInt(id);
+
+              if (id != null) {
+                tirocinioModel = new TirocinioModel();
+
+                Tirocinio tirocinio = (Tirocinio) tirocinioModel.doRetrieveByKey(idInt);
+
+                if (tirocinio.getStato().equalsIgnoreCase("Accettata")) {
+                  tirocinioModel.doModify("Confermata",idInt);
+                  request.setAttribute("message_success_training", "Richiesta"
+                      + " di Tirocinio è stata Confermata.");
+                } else {
+                  request.setAttribute("message_fault_training", "Per confermare "
+                      + "la richiesta di Tirocinio lo stato deve essere in Accettata.");
+                }
+
+                returnPath = "/MyTraining.jsp";
+              }
+            }
+          }
+        } else if (action.equalsIgnoreCase("reject_student_training_student")) {
+          Studente sessioneStudent = (Studente) request.getSession().getAttribute("student");
+
+          if (sessioneStudent != null) {
+            if (sessioneStudent.getEmail().length() > 0) {
+              String id = request.getParameter("id");
+              int idInt = Integer.parseInt(id);
+
+              if (id != null) {
+                tirocinioModel = new TirocinioModel();
+
+                Tirocinio tirocinio = (Tirocinio) tirocinioModel.doRetrieveByKey(idInt);
+
+                if (tirocinio.getStato().equalsIgnoreCase("Accettata")) {
+                  tirocinioModel.doModify("Rifiutata dallo Studente",idInt);
+                  request.setAttribute("message_success_training", "Richiesta "
+                      + "di Tirocinio è stata Rifiutata.");
+                } else {
+                  request.setAttribute("message_fault_training", "Per rifiutare "
+                      + "la richiesta di Tirocinio lo stato deve essere in Accettata.");
+                }
+
+                returnPath = "/MyTraining.jsp";
+              }
+            }
+          }
+        } else if (action.equalsIgnoreCase("accept_training_student")) {
+          ProfessoreTutorAziendale sessioneTeacher = (ProfessoreTutorAziendale) 
+              request.getSession().getAttribute("teacher");
+          ProfessoreTutorAziendale sessioneTutor = (ProfessoreTutorAziendale) 
+              request.getSession().getAttribute("tutor");
+
+          boolean control = false;
+
+          if (sessioneTeacher != null) {
+            if (sessioneTeacher.getEmail().length() > 0) {
+              control = true;
+            }
+          }
+
+          if (sessioneTutor != null) {
+            if (sessioneTutor.getEmail().length() > 0) {
+              control = true;
+            }
+          }
+
+          if (control) {
+            String id = request.getParameter("id");
+            int idInt = Integer.parseInt(id);
+
+            if (id != null) {
+              tirocinioModel = new TirocinioModel();
+
+              Tirocinio tirocinio = (Tirocinio) tirocinioModel.doRetrieveByKey(idInt);
+
+              if (tirocinio.getStato().equalsIgnoreCase("In Attesa")) {
+                tirocinioModel.doModify("Accettata",idInt);
+                request.setAttribute("message_success_training", "Richiesta "
+                    + "di Tirocinio Accettata.");
+              } else {
+                request.setAttribute("message_fault_training", "Per accettare"
+                    + " la richiesta di Tirocinio lo stato deve essere In Attesa.");
+              }
+
+              returnPath = "/RequestTraining.jsp";
+            }
+          }
+        } else if (action.equalsIgnoreCase("reject_training_student")) {
+          ProfessoreTutorAziendale sessioneTeacher = 
+              (ProfessoreTutorAziendale) request.getSession().getAttribute("teacher");
+          ProfessoreTutorAziendale sessioneTutor = 
+              (ProfessoreTutorAziendale) request.getSession().getAttribute("tutor");
+
+          boolean control = false;
+
+          if (sessioneTeacher != null) {
+            if (sessioneTeacher.getEmail().length() > 0) {
+              control = true;
+            }
+          }
+
+          if (sessioneTutor != null) {
+            if (sessioneTutor.getEmail().length() > 0) {
+              control = true;
+            }
+          }
+
+          if (control) {
+            String id = request.getParameter("id");
+            int idInt = Integer.parseInt(id);
+
+            if (id != null) {
+              tirocinioModel = new TirocinioModel();
+
+              Tirocinio tirocinio = (Tirocinio) tirocinioModel.doRetrieveByKey(idInt);
+             
+              if (tirocinio.getStato().equalsIgnoreCase("In Attesa")) {
+                tirocinioModel.doModify("Rifiutata",idInt);
+                request.setAttribute("message_success_training", "Richiesta"
+                    + " di Tirocinio Rifiutata.");
+              } else {
+                request.setAttribute("message_fault_training", "Per "
+                    + "rifiutare la richiesta di Tirocinio lo stato deve essere In Attesa.");
+              }
+
+              returnPath = "/RequestTraining.jsp";
+            }
+          }
+        } else if (action.equalsIgnoreCase("convalidate_training_student")) {
+          Segreteria sessioneSegreteria =
+                (Segreteria) request.getSession().getAttribute("segreteria");
+
+          boolean control = false;
+
+          if (sessioneSegreteria != null) {
+            if (sessioneSegreteria.getEmail().length() > 0) {
+              control = true;
+            }
+          }
+
+          if (control) {
+            String id = request.getParameter("id");
+            int idInt = Integer.parseInt(id);
+
+            if (id != null) {
+              tirocinioModel = new TirocinioModel();
+
+              Tirocinio tirocinio = (Tirocinio) tirocinioModel.doRetrieveByKey(idInt);
+
+              if (tirocinio.getStato().equalsIgnoreCase("Completato")) {
+                tirocinioModel.doModify("Convalidato",idInt);
+                request.setAttribute("message_success_training", "Tirocinio Convalidato.");
+              } else {
+                request.setAttribute("message_fault_training", "Per convalidare "
+                    + "il Tirocinio lo stato deve essere in Completato.");
+              }
+
+              returnPath = "/segreteria/CompletedTraining.jsp";
+            }
+          }
+        }
+      }
+    } catch (SQLException e) {
+      System.out.println("Error:" + e.getMessage());
+    }
+
+    RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(returnPath);
+    dispatcher.forward(request, response);
+  }
+
+  /**
+  * Il metodo riceve una richiesta dal client e 
+  * richiama il medoto doGet(request, response) per svolgere la procedura aspettata.
+  */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
